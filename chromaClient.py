71c6e3b8d209d1d6c35a72047dc41016b4edfd2d
ntua-el19219 <el19219@mail.ntua.gr>
2025-07-03 17:42:35 +0300

    Fix Chroma import again

--- chromaClient.py
+++ chromaClient.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 import chromadb
 
